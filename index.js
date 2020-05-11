c9579f1fe0985eae087027f4716b225156fcf6a5
Paul Hachmang <paul@reachdigital.nl>
2020-05-11 21:35:22 +0200

    fix: Cannot read property '0' of null

--- index.js
+++ index.js
@@ -6,26 +6,22 @@ import { get } from "lodash-es";
 let token, octokit, branch, repo, owner, retryInterval;
 
 async function getDeployment(args) {
-  await new Promise((resolve) => setTimeout(resolve, retryInterval));
-  let result = await octokit.graphql(query, args);
-  let environment = get(
-    result,
-    "repository.ref.target.deployments.edges[0].node.latestStatus.environmentUrl",
-    null
-  );
+  environment = null
   while (!environment) {
-    console.log(
-      `environment is null, waiting ${retryInterval} milliseconds and trying again`
-    );
+    environment = await tryGetResult(args)
+    if (!environment) {
+      console.log(`environment is null, waiting ${retryInterval} milliseconds and trying again`)
+    }
     await new Promise((resolve) => setTimeout(resolve, retryInterval));
-    result = await octokit.graphql(query, args);
-    environment = get(
-      result,
-      "repository.ref.target.deployments.edges[0].node.latestStatus.environmentUrl",
-      null
-    );
   }
   return result;
+}
+
+async function tryGetResult() {
+  result = await octokit.graphql(query, args);
+  edges = get(result, "repository.ref.target.deployments.edges")
+  if (!edges) return null
+  return get(edges, `[0].node.latestStatus.environmentUr`, null);
 }
 
 async function run() {
