89c72431d5fa5ee58b4baa66702d0d28cdf9355a
Paul Hachmang <paul@reachdigital.nl>
2020-05-12 09:34:20 +0200

    use GITHUB_HEAD_REF instead of GITHUB_REF

--- dist/index.js
+++ dist/index.js
@@ -27597,7 +27597,7 @@ async function tryGetResult(args) {
 async function run() {
   try {
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    const branch = process.env.GITHUB_REF.match(/(?<=refs\/heads\/).+/g)[0];
+    const branch = process.env.GITHUB_HEAD_REF.match(/(?<=refs\/heads\/).+/g)[0];
     const retryInterval = Number(Object(core.getInput)("retryInterval"))
 
     const args = { repo, owner, branch };
