1d5ef563ef473be4964b7215329c2ae01dcb2c86
Dor Shinar <dors718@gmail.com>
2020-07-14 11:53:21 +0300

    feat: wait for GitHub API rate limit to reset if exceeded

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 import { getInput, setOutput, setFailed } from "@actions/core";
 import { GitHub } from "@actions/github";
-import query from "./query.gql";
 import { get } from "lodash-es";
+import query from "./query.gql";
 
 async function getDeployment(args, retryInterval) {
-  let environment = null
+  let environment = null;
   while (!environment) {
-    environment = await tryGetResult(args)
-    if (!environment) 
-      console.log(`environment is null, waiting ${retryInterval} milliseconds and trying again`)
+    environment = await tryGetResult(args);
+    if (!environment)
+      console.log(
+        `environment is null, waiting ${retryInterval} milliseconds and trying again`
+      );
     await new Promise((resolve) => setTimeout(resolve, retryInterval));
   }
   return environment;
 }
 
 async function tryGetResult(args) {
-  const octokit = new GitHub(getInput("token", { required: true }))
+  const octokit = new GitHub(getInput("token", { required: true }));
   const result = await octokit.graphql(query, args);
-  const edges = get(result, "repository.ref.target.deployments.edges")
-  if (!edges) return null
+  await waitForRateLimitReset(result);
+
+  const edges = get(result, "repository.ref.target.deployments.edges");
+  if (!edges) return null;
   return get(edges, `[0].node.latestStatus.environmentUrl`, null);
+}
+
+async function waitForRateLimitReset(result) {
+  const { cost, remaining, resetAt } = result.rateLimit;
+  if (remaining >= cost) return;
+
+  const timeToRateLimitReset =
+    new Date(resetAt).getTime() - new Date().getTime();
+  await new Promise((resolve) => setTimeout(resolve, timeToRateLimitReset));
 }
 
 async function run() {
   try {
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    const branch = process.env.GITHUB_HEAD_REF ||
-      process.env.GITHUB_REF.match(/(?<=refs\/heads\/).+/g)[0] 
-    const retryInterval = Number(getInput("retryInterval"))
+    const branch =
+      process.env.GITHUB_HEAD_REF ||
+      process.env.GITHUB_REF.match(/(?<=refs\/heads\/).+/g)[0];
+    const retryInterval = Number(getInput("retryInterval"));
 
     const args = { repo, owner, branch };
     console.log("Starting to run with following input:", args);
